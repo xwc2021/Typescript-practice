7d5a0f907162cb89ef1cc6c9e774a3366466a288
xwc2021 <xiangwei71@gmail.com>
2021-12-01 15:45:09 +0800

    改回NW、NE、SW、SE：決定對uv做變換就好，維持buffer space左上為(0,0)

--- src/Math/Sampler.ts
+++ src/Math/Sampler.ts
@@ -7,9 +7,8 @@ import { MathHelper } from "./Tool";
 export default class Sampler {
 
     static texture2D(u: number, v: number, w: number, h: number, buffer: Buffer2D<RGBA>) {
+
         //先找出最近點
-        // let grid_u = MathHelper.accDiv(1, w);
-        // let grid_v = MathHelper.accDiv(1, h);
         let grid_u = 1 / w;
         let grid_v = 1 / h;
 
@@ -17,8 +16,6 @@ export default class Sampler {
         let half_grid_v = grid_v * 0.5;
 
         //以下是有4個鄰點的情況..
-        // let nearest_point_u_float = MathHelper.accDiv(u, grid_u);
-        // let nearest_point_v_float = MathHelper.accDiv(v, grid_v);
         let nearest_point_u_float = u / grid_u;
         let nearest_point_v_float = v / grid_v;
 
@@ -32,7 +29,7 @@ export default class Sampler {
         let s_v = v % grid_v;
 
         //再找出相鄰3點
-        if (s_u >= half_grid_u && s_v >= half_grid_v)//相鄰3點在右上
+        if (s_u >= half_grid_u && s_v >= half_grid_v)//相鄰3點在右下
         {
             //剛好整除時要做修正
             if (nearest_point_u_float == nearest_point_u)
@@ -40,75 +37,71 @@ export default class Sampler {
 
             if (nearest_point_v_float == nearest_point_v)
                 nearest_point_v = nearest_point_v - 1;
-            let P = new Vector2D(nearest_point_u, nearest_point_v);
-
-            // 右上
-            // console.log('右上', u, v, P);
-            let BR = new Vector2D(P.x + 1, P.y);
-            let TL = new Vector2D(P.x, P.y + 1);
-            let TR = new Vector2D(P.x + 1, P.y + 1);
-            //在4點內的uv
-            let rectUV = new Vector2D((s_u - half_grid_u) / grid_u, (s_v - half_grid_v) / grid_v);
-            return { rectUV, BL: P, BR, TL, TR, color: Sampler.Bilinear_Sampler(rectUV, P, BR, TL, TR, buffer) };
-        }
-        else if (s_u <= half_grid_u && s_v >= half_grid_v)//相鄰3點在左上
-        {
-            //剛好整除時要做修正
-            if (nearest_point_v_float == nearest_point_v)
-                nearest_point_v = nearest_point_v - 1;
-            let P = new Vector2D(nearest_point_u, nearest_point_v);
-
-            // 左上
-            // console.log('左上', u, v, P);
-            let BL = new Vector2D(P.x - 1, P.y);
-            let TL = new Vector2D(P.x - 1, P.y + 1);
-            let TR = new Vector2D(P.x, P.y + 1);
-            //在4點內的uv
-            let rectUV = new Vector2D((s_u + half_grid_u) / grid_u, (s_v - half_grid_v) / grid_v);
-            return { rectUV, BL, BR: P, TL, TR, color: Sampler.Bilinear_Sampler(rectUV, BL, P, TL, TR, buffer) };
-        }
-        else if (s_u <= half_grid_u && s_v <= half_grid_v)//相鄰3點在左下
-        {
-            let P = new Vector2D(nearest_point_u, nearest_point_v);
-
-            // 左下
-            // console.log('左下', u, v, P);
-            let BL = new Vector2D(P.x - 1, P.y - 1);
-            let BR = new Vector2D(P.x, P.y - 1);
-            let TL = new Vector2D(P.x - 1, P.y);
-            //在4點內的uv
-            let rectUV = new Vector2D((s_u + half_grid_u) / grid_u, (s_v + half_grid_v) / grid_v);
-            return { rectUV, BL, BR, TL, TR: P, color: Sampler.Bilinear_Sampler(rectUV, BL, BR, TL, P, buffer) };
-        }
-        else if (s_u >= half_grid_u && s_v <= half_grid_v)//相鄰3點在右下
-        {
-            //剛好整除時要做修正
-            if (nearest_point_u_float == nearest_point_u)
-                nearest_point_u = nearest_point_u - 1;
             let P = new Vector2D(nearest_point_u, nearest_point_v);
 
             // 右下
-            // console.log('右下', u, v, P);
-            let BL = new Vector2D(P.x, P.y - 1);
-            let BR = new Vector2D(P.x + 1, P.y - 1);
-            let TR = new Vector2D(P.x + 1, P.y);
+            let NE = new Vector2D(P.x + 1, P.y);
+            let SW = new Vector2D(P.x, P.y + 1);
+            let SE = new Vector2D(P.x + 1, P.y + 1);
+            //在4點內的uv
+            let rectUV = new Vector2D((s_u - half_grid_u) / grid_u, (s_v - half_grid_v) / grid_v);
+            return { rectUV, NW: P, NE, SW, SE, color: Sampler.Bilinear_Sampler(rectUV, P, NE, SW, SE, buffer) };
+        }
+        else if (s_u <= half_grid_u && s_v >= half_grid_v)//相鄰3點在左下
+        {
+            //剛好整除時要做修正
+            if (nearest_point_v_float == nearest_point_v)
+                nearest_point_v = nearest_point_v - 1;
+            let P = new Vector2D(nearest_point_u, nearest_point_v);
+
+            // 左下
+            let NW = new Vector2D(P.x - 1, P.y);
+            let SW = new Vector2D(P.x - 1, P.y + 1);
+            let SE = new Vector2D(P.x, P.y + 1);
+            //在4點內的uv
+            let rectUV = new Vector2D((s_u + half_grid_u) / grid_u, (s_v - half_grid_v) / grid_v);
+            return { rectUV, NW, NE: P, SW, SE, color: Sampler.Bilinear_Sampler(rectUV, NW, P, SW, SE, buffer) };
+        }
+        else if (s_u <= half_grid_u && s_v <= half_grid_v)//相鄰3點在左上
+        {
+            let P = new Vector2D(nearest_point_u, nearest_point_v);
+
+            // 左上
+            let NW = new Vector2D(P.x - 1, P.y - 1);
+            let NE = new Vector2D(P.x, P.y - 1);
+            let SW = new Vector2D(P.x - 1, P.y);
+            //在4點內的uv
+            let rectUV = new Vector2D((s_u + half_grid_u) / grid_u, (s_v + half_grid_v) / grid_v);
+            return { rectUV, NW, NE, SW, SE: P, color: Sampler.Bilinear_Sampler(rectUV, NW, NE, SW, P, buffer) };
+        }
+        else if (s_u >= half_grid_u && s_v <= half_grid_v)//相鄰3點在右上
+        {
+            //剛好整除時要做修正
+            if (nearest_point_u_float == nearest_point_u)
+                nearest_point_u = nearest_point_u - 1;
+            let P = new Vector2D(nearest_point_u, nearest_point_v);
+
+            // 右上
+            let NW = new Vector2D(P.x, P.y - 1);
+            let NE = new Vector2D(P.x + 1, P.y - 1);
+            let SE = new Vector2D(P.x + 1, P.y);
             //在4點內的uv
             let rectUV = new Vector2D((s_u - half_grid_u) / grid_u, (s_v + half_grid_v) / grid_v);
-            return { rectUV, BL, BR, TL: P, TR, color: Sampler.Bilinear_Sampler(rectUV, BL, BR, P, TR, buffer) };
+            return { rectUV, NW, NE, SW: P, SE, color: Sampler.Bilinear_Sampler(rectUV, NW, NE, P, SE, buffer) };
         }
     }
 
-    static Bilinear_Sampler(rectUV: Vector2D, BL: Vector2D, BR: Vector2D, TL: Vector2D, TR: Vector2D, buffer: Buffer2D<RGBA>) {
+    static Bilinear_Sampler(rectUV: Vector2D, NW: Vector2D, NE: Vector2D, SW: Vector2D, SE: Vector2D, buffer: Buffer2D<RGBA>) {
 
         //對4個點顏色作內插
-        let BLc = buffer.get_clamp_mode(BL.x, BL.y);
-        let BRc = buffer.get_clamp_mode(BR.x, BR.y);
-        let TLc = buffer.get_clamp_mode(TL.x, TL.y);
-        let TRc = buffer.get_clamp_mode(TR.x, TR.y);
+        let NWc = buffer.get_clamp_mode(NW.x, NW.y);
+        let NEc = buffer.get_clamp_mode(NE.x, NE.y);
+        let SWc = buffer.get_clamp_mode(SW.x, SW.y);
+        let SEc = buffer.get_clamp_mode(SE.x, SE.y);
 
-        let bottomRGB = RGBA.lerp(BLc, BRc, rectUV.x);
-        let topRGB = RGBA.lerp(TLc, TRc, rectUV.x);
-        let middleRGB = RGBA.lerp(bottomRGB, topRGB, rectUV.y);
+        let nRGB = RGBA.lerp(NWc, NEc, rectUV.x);
+        let sRGB = RGBA.lerp(SWc, SEc, rectUV.x);
+        let middleRGB = RGBA.lerp(nRGB, sRGB, rectUV.y);
         return middleRGB;
     }
 
